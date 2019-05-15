7d4b161a90dad9afb7785c39e3df017dacc0e5c3
Kyran <krs19@live.com>
2019-05-16 01:59:34 +1200

    completed story's 5,6,7,8 & 9. also did some styling
    although the style is garbage

--- assignment2-client/src/Venue.vue
+++ assignment2-client/src/Venue.vue
@@ -1,45 +1,79 @@
 <template>
   <div id="venuesPage">
-    <h1>Venues</h1>
-    <br>
-    <br>
-    <div id="Venues">
-      <!-- filtering options -->
-      <div id="filters">
-        Search
-        <input v-model="filterVenueName" placeholder="Search" @input="getVenues()">
-        Filter by city:
-        <select name="cityFilter" @change="onCityFilterChange($event)">
-          <option selected="selected" value>No filter</option>
-          <option v-for="city in cities" :value="city" v-bind:key="city">{{ city }}</option>
-        </select>
-        Filter by category:
-        <select
-          name="categoryFilter"
-          @change="onCategoryFilterChange($event)"
-        >
-          <option selected="selected" value>No filter</option>
-          <option
-            v-for="category in categories"
-            :value="category.categoryId"
-            v-bind:key="category.categoryId"
-          >{{ category.categoryName }}</option>
-        </select>
-        Order:
-        <select name="orderFilter" id="orderFilter" @change="onOrderFilterChange($event)">
-          <option selected="selected" value=1>highest to lowest average (mean) star rating</option>
-          <option value=2>lowest to highest average (mean) star rating</option>
-          <option value=3>highest to lowest average (mode) cost rating</option>
-          <option value=4>lowest to highest average (mode) cost rating</option>
-        </select>
+    <!-- filtering options -->
+    <div id="filters">
+      <h1>Venues</h1>
+      <p>Search</p>
+      <input id="venueSearch" v-model="filterVenueName" placeholder="Search" @input="getVenues()">
+      <p>Filter by city:</p>
+      <select name="cityFilter" @change="onCityFilterChange($event)">
+        <option selected="selected" value>No filter</option>
+        <option v-for="city in cities" :value="city" v-bind:key="city">{{ city }}</option>
+      </select>
+      <p>Filter by category:</p>
+      <select name="categoryFilter" @change="onCategoryFilterChange($event)">
+        <option selected="selected" value>No filter</option>
+        <option
+          v-for="category in categories"
+          :value="category.categoryId"
+          v-bind:key="category.categoryId"
+        >{{ category.categoryName }}</option>
+      </select>
+      <p>Order:</p>
+      <select name="orderFilter" id="orderFilter" @change="onOrderFilterChange($event)">
+        <option selected="selected" value="1">Highest to lowest star rating</option>
+        <option value="2">Lowest to highest star rating</option>
+        <option value="3">Highest to lowest cost rating</option>
+        <option value="4">Lowest to highest cost rating</option>
+        <option value="5" id="closestDistance">Closest to me</option>
+        <option value="6" id="furthestDistance">Furthest from me</option>
+      </select>
+
+      <div id="starFilter">
+        <p>Minimum Star Average</p>
+        <input class="starFilterButton" type="radio" name="noStarFilter" id="noStarFilter" value="0" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="noStarFilter">None</label>
+        <input class="starFilterButton" type="radio" name="oneStar" id="oneStar" value="1" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="oneStar">1</label>
+        <input class="starFilterButton" type="radio" name="twoStar" id="twoStar" value="2" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="twoStar">2</label>
+        <input class="starFilterButton" type="radio" name="threeStar" id="threeStar" value="3" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="threeStar">3</label>
+        <input class="starFilterButton" type="radio" name="fourStar" id="fourStar" value="4" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="fourStar">4</label>
+        <input class="starFilterButton" type="radio" name="fiveStar" id="fiveStar" value="5" v-model="minimumStarRating" v-on:change="getVenues()">
+        <label for="fiveStar">5</label>
       </div>
 
+      <div id="starFilter">
+        <p>Cost Rating</p>
+        <input class="costFilterButton" type="radio" name="noFilter" id="noFilter" value="5" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="noFilter">None</label>
+        <input class="costFilterButton" type="radio" name="freeCost" id="freeCost" value="0" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="freeCost">Free</label>
+        <input class="costFilterButton" type="radio" name="oneCost" id="oneCost" value="1" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="oneCost">1</label>
+        <input class="costFilterButton" type="radio" name="twoCost" id="twoCost" value="2" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="twoCost">2</label>
+        <input class="costFilterButton" type="radio" name="threeCost" id="threeCost" value="3" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="threeCost">3</label>
+        <input class="costFilterButton" type="radio" name="fourCost" id="fourCost" value="4" v-model="maxCostRating" v-on:change="getVenues()">
+        <label for="fourCost">4</label>
+      </div>
 
+      <div id="forwardBack">
+        <a id="nextLink" @click="nextVenues()">next</a>
+        <span>Viewing: {{ displayVenuesCount() }}</span>
+        <a id="previousLink" @click="previousVenues()">back</a>
+      </div>
+    </div>
+
+    <div id="Venues">
       <!-- display table of venues -->
       <div>
-        <td class="venueTable" v-for="venue in venues" v-bind:key="venue.id">
+        <div class="venueTable" v-for="venue in venues" v-bind:key="venue.id">
           <div class="venuePhoto">
-            <img v-bind:src="getVenuePrimaryImage(venue)" height="100" width="100">
+            <img v-bind:src="getVenuePrimaryImage(venue)" id="venuePrimaryThumbnail">
           </div>
           <div class="venueDetails">
             {{ venue.venueName }}
@@ -48,7 +82,7 @@
             <br>
             ★{{ venue.meanStarRating}} ${{venue.modeCostRating}}
           </div>
-        </td>
+        </div>
       </div>
     </div>
   </div>
@@ -71,13 +105,35 @@ export default {
       defaultVenuePhoto: defaultPhoto, //the default photo for when a venue has no photos
       filterVenueName: "", //the name search to filter by
       sortBy: "STAR_RATING", //sort by star_rating or cost_rating
-      reversedOrder: false //reverse the order of the venues
+      reversedOrder: false, //reverse the order of the venues
+      geolocationEnabled: false, //tells the program if location is set
+      lat: "",
+      lon: "",
+      venuePage: 1,
+      minimumStarRating: 0,
+      maxCostRating: 5
     };
+  },
+  beforeMount() {
+    this.geolocation();
   },
   mounted: function() {
     this.getVenues(), this.getCategories();
   },
   methods: {
+    geolocation() {
+      navigator.geolocation.getCurrentPosition(this.buildUrl, this.geoError);
+    },
+    buildUrl(position) {
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+      this.geolocationEnabled = true;
+    },
+    geoError(error) {
+      this.geolocationEnabled = false;
+      document.getElementById("closestDistance").disabled = true;
+      document.getElementById("furthestDistance").disabled = true;
+    },
     getVenues: function() {
       //gets the venues
       var uri = this.serverAddress + "venues/"; //prepare the uri with filters
@@ -88,36 +144,61 @@ export default {
       }
       if (this.filterVenueName !== "") {
         if (!questionMarkFlag) {
-          uri = uri + "?q=" + this.filterVenueName; 
-          questionMarkFlag = true;}
-        else uri = uri + "&q=" + this.filterVenueName;
+          uri = uri + "?q=" + this.filterVenueName;
+          questionMarkFlag = true;
+        } else uri = uri + "&q=" + this.filterVenueName;
       }
       if (this.filterCategory !== "") {
         if (!questionMarkFlag) {
           uri = uri + "?categoryId=" + this.filterCategory;
           questionMarkFlag = true;
-        }
-        else uri = uri + "&categoryId=" + this.filterCategory;
+        } else uri = uri + "&categoryId=" + this.filterCategory;
       }
       if (this.sortBy !== "STAR_RATING") {
         if (!questionMarkFlag) {
           uri = uri + "?sortBy=" + this.sortBy;
           questionMarkFlag = true;
-        }
-        else uri = uri + "&sortBy=" + this.sortBy;
+        } else uri = uri + "&sortBy=" + this.sortBy;
       }
-      if (this.reversedOrder){
+      if (this.reversedOrder) {
         if (!questionMarkFlag) {
           uri = uri + "?reverseSort=" + this.reversedOrder;
           questionMarkFlag = true;
-        }
-        else uri = uri + "&reverseSort=" + this.reversedOrder;
+        } else uri = uri + "&reverseSort=" + this.reversedOrder;
+      }
+      if (this.sortBy === "DISTANCE") {
+        uri = uri + "&myLatitude=" + this.lat;
+        uri = uri + "&myLongitude=" + this.lon;
+      }
+      if (this.venuePage !== 1) {
+        var index = (this.venuePage - 1) * 10;
+        if (!questionMarkFlag) {
+          uri = uri + "?startIndex=" + index;
+          questionMarkFlag = true;
+        } else uri = uri + "&startIndex=" + index;
+      }
+      if (!questionMarkFlag) {
+        uri = uri + "?count=" + 10;
+        questionMarkFlag = true;
+      } else uri = uri + "&count=" + 10;
+      if (this.minimumStarRating != 0) {
+        if (!questionMarkFlag) {
+          uri = uri + "?minStarRating=" + this.minimumStarRating;
+          questionMarkFlag = true;
+        } else uri = uri + "&minStarRating=" + this.minimumStarRating;
+      }
+      if (this.maxCostRating != 5) {
+        if (!questionMarkFlag) {
+          uri = uri + "?maxCostRating=" + this.maxCostRating;
+          questionMarkFlag = true;
+        } else uri = uri + "&maxCostRating=" + this.maxCostRating;
       }
 
       // make the request
       this.$http.get(uri).then(
         function(response) {
           this.venues = response.data;
+          console.log(this.venues)
           this.getCities(); //Needs to be run once venues are got correctly, not when mounted
         },
         function(error) {
@@ -168,6 +249,13 @@ export default {
         }
       }
     },
+    displayVenuesCount: function() {
+      var str = "";
+      str = str + ((this.venuePage - 1) * 10 + 1);
+      str = str + " - ";
+      str = str + ((this.venuePage - 1) * 10 + this.venues.length);
+      return str;
+    },
     onCityFilterChange: function(event) {
       //change the filter city when the drop down is changed
       this.filterCity = event.target.value;
@@ -196,7 +284,27 @@ export default {
         this.sortBy = "COST_RATING";
         this.reversedOrder = false;
       }
+      if (event.target.value === "5") {
+        this.sortBy = "DISTANCE";
+        this.reversedOrder = false;
+      }
+      if (event.target.value === "6") {
+        this.sortBy = "DISTANCE";
+        this.reversedOrder = true;
+      }
       this.getVenues();
+    },
+    nextVenues: function() {
+      if (this.venues.length === 10) {
+        this.venuePage++;
+        this.getVenues();
+      }
+    },
+    previousVenues: function() {
+      if (this.venuePage != 1) {
+        this.venuePage--;
+        this.getVenues();
+      }
     }
   }
 };
