4aebe8a418625e9f84c1467653ffdc9223f1bc5e
Unknown <kst84@uclive.ac.nz>
2019-05-20 16:23:19 +1200

    Completed story 10
    ew

--- assignment2-client/src/Venue.vue
+++ assignment2-client/src/Venue.vue
@@ -1,28 +1,104 @@
 <template>
-    <div id="Page">
-        <div id="sidebar">
-            <router-link to="/">
-            <a>Back to Venues</a>
-            </router-link>
-            
-            <div id="basicVenue">
-                
-            </div>
-        </div>
+  <div v-if="checkLoading()">
+    <h1>Loading</h1>
+  </div>
+  <div v-else id="Page">
+    <div id="sidebar">
+      <router-link to="/">
+        <a>Back to Venues</a>
+      </router-link>
 
-        <div id="mainBody">
-            <h1> {{ $route.params.venueid }} </h1>
-        </div>
+      <div id="basicVenue">
+        <h1>{{ venue.venueName }}</h1>
+        <p>{{ venue.category.categoryName }}</p>
+        <p>Admin: {{ venue.admin.username }}</p>
+        <p>City: {{ venue.city }}</p>
+        <p>{{ venue.address }}</p>
+        <p>Date Added: {{ venue.dateAdded.split("T")[0] }}</p>
+        <h3>Star Rating: {{ starRating }}</h3>
+        <h3>Cost Rating: {{ costRating }}</h3>
+      </div>
     </div>
+
+    <div id="mainBody">
+      <div class="wrapper-parent">
+        <carousel paginationActiveColor="#73c000" autoplayTimeout:1000>
+          <slide v-for="image in images" v-bind:key="image">
+            <img :src="getVenueImageUrl(image)" height="400px">
+          </slide>
+        </carousel>
+      </div>
+      <div id="description">
+        <p>{{ venue.shortDescription }}</p>
+        <p style="display: none" id="longDescription">{{ venue.longDescription }}</p>
+        <a v-on:click="showHideDescription()">{{ longDescOutput }}</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
     data() {
         return {
-            venue: {},
-            venueid: null
+            error: "",
+            errorFlag: false,
+            venue: {
+            "venueName": "",
+            "admin": {
+                "userId": "",
+                "username": ""
+            },
+            "category": {
+                "categoryId": "",
+                "categoryName": "",
+                "categoryDescription": ""
+            },
+            "city": "",
+            "shortDescription": "",
+            "longDescription": "",
+            "dateAdded": "",
+            "address": "",
+            "latitude": "",
+            "longitude": "",
+            "photos": []
+            },
+            venueid: this.$route.params.venueid,
+            images: [],
+            index: null,
+            link: "http://localhost:4941/api/v1/venues/" + this.$route.params.venueid + "/photos/",
+            descriptionShown: false,
+            longDescOutput: "More",
+            costRating: 0,
+            starRating: 0
         };
+    },
+    watch: {
+      '$route'() {
+        this.venue = {
+            "venueName": "",
+            "admin": {
+                "userId": "",
+                "username": ""
+            },
+            "category": {
+                "categoryId": "",
+                "categoryName": "",
+                "categoryDescription": ""
+            },
+            "city": "",
+            "shortDescription": "",
+            "longDescription": "",
+            "dateAdded": "",
+            "address": "",
+            "latitude": "",
+            "longitude": "",
+            "photos": []
+            };
+        this.getVenue();
+      }
     },
     beforeMount: function() {
         if(this.$route.params.venueid === null) {
@@ -31,9 +107,12 @@ export default {
         }
     },
     mounted: function() {
-        this.getVenue();
+        this.getVenue()
     },
     methods: {
+        checkLoading: function() {
+            return (this.venue.venueName == "");
+        },
         getVenue: function() {
             this.$http.get(this.serverAddress + 'venues/' + this.$route.params.venueid)
             .then(function(response) {
@@ -44,11 +123,65 @@ export default {
                 this.error = error;
                 this.errorFlag = true;
             }).then(function(){
+                for (var i = 0; i < this.venue.photos.length; i++) {
+                    this.images.push(this.venue.photos[i].photoFilename)
+                }
                 if(this.venueid === null) {
                     this.$router.push( { name: "Venues" } )
                 }
+                this.getStarRating()
+                this.getCostRating()
             });
-        }
+        },
+        getVenueImageUrl: function(photo) {
+        //gets the primary image get uri of a venue
+        return (this.link + photo);
+        },
+        showHideDescription: function() {
+            
+            if (this.descriptionShown) {
+                document.getElementById("longDescription").style.display = "none"
+                this.descriptionShown = false;
+                this.longDescOutput = "More"
+            } else {
+                document.getElementById("longDescription").style.display = "block"
+                this.descriptionShown=true;
+                this.longDescOutput = "Less"
+            }
+        },
+        getStarRating: function(){
+            this.$http.get(this.serverAddress + "venues/").then(
+            function(response) {
+                for (var i = 0; i < response.data.length; i++){
+                    if (response.data[i].venueId == this.venueid) {
+                        this.starRating = response.data[i].meanStarRating
+                    }
+                }
+            },
+            function(error) {
+                this.error = error;
+                this.errorFlag = true;
+            }
+        )},
+        getCostRating: function(){
+            this.$http.get(this.serverAddress + "venues/").then(
+            function(response) {
+                for (var i = 0; i < response.data.length; i++){
+                    if (response.data[i].venueId == this.venueid) {
+                        this.costRating = response.data[i].modeCostRating
+                        break;
+                    }
+                }
+            },
+            function(error) {
+                this.error = error;
+                this.errorFlag = true;
+            }
+        )},
+    },
+    components: {
+      Carousel,
+      Slide
     }
 }
 </script>
